dd4dac5024af6b9328c2940759166cc0450814cc
Emil Terman <terman.emil@gmail.com>
2018-04-03 18:46:50 +0300

    I kind of started doing lab3

---
+++ lab3/Cmagru42/Cmagru42/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models.AccountViewModels;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("Register")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            System.Diagnostics.Debug.WriteLine(ViewData);
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(
+            RegisterViewModel model,
+            string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                //var user 
+            }
+            else
+            {
+                // Go back if something failed.
+                return View(model);   
+            }
+        }
+    }
+}
---
+++ lab3/Cmagru42/Cmagru42/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(
+            30,
+            MinimumLength = 3,
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(
+            124,
+            MinimumLength = 6,
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation code don't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
